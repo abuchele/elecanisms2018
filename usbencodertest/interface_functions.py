6ee2ce5df9a0fbc5eb8ba50aa8342063367c64fe
abuchele <anna.buchele@students.olin.edu>
2018-02-25 20:06:45 -0500

    commenting stuff

--- usbencodertest/interface_functions.py
+++ usbencodertest/interface_functions.py
@@ -26,22 +26,45 @@ DELTA_ANGLE_THRESH = 0.5
 
 BASE_MOTOR_SPEED = 20
 
+""" Direction key:
+
+Angle+ is turning clockwise ("forward")
+Angle- is turning counter-clockwise ("backwards")
+Angle limits of paddle (if centered on zero) is -800 to 800
+
+Direction =  0 - stopped
+Direction =  1 - forward
+Direction = -1 - backward
+
+intended_direction = direction we want to be moving
+motor_direction = direction the motor is currently moving
+direction = angular direction we are currently moving
+
+motor_direction and direction are different in that with user turning, the angular direction 
+	we are moving could be different from the direction the motor is turning. 
+
+"""
+
 
 class user_functions:
 	def __init__(self):
+		""" Initializing class variables
+		"""
 		self.joystick = usbencodertest()
+		self.clear_motor()
 		self.mode = 0
 		self.min_angle = -800
 		self.max_angle = 800
-		self.clear_motor()
 		self.direction = 0
 		self.motor_direction = 0
+		self.intended_direction = 0
 		self.motor_speed = 0
 		self.del_angle = 0
 		self.joystick.delta_angle = 0
-		self.intended_direction = 0
 
 	def update_vals(self):
+		""" Should be called each cycle. Updates class variables with new values. 
+		"""
 		self.angle = self.joystick.track_angle()
 		self.del_angle = self.joystick.delta_angle
 		if self.del_angle > DELTA_ANGLE_THRESH:
@@ -53,35 +76,48 @@ class user_functions:
 		self.a0 = self.joystick.read_a0()
 
 	def write_motor(self,forward,backward):
+		""" Writes the set duty cycle to the motor. This function should not be directly used, 
+				instead use write_forward, write_backward, or clear_motor as needed. 
+		"""
 		self.joystick.set_duty(forward)
 		self.joystick.set_duty_rev(backward)
 		self.curr_drive = [forward,backward]
 		time.sleep(MOTOR_SLEEP_TIME)
 
 	def write_forward(self,val):
+		""" Writes the set duty cycle to the motor in the forward direction. Val should be between 0-100. 
+		"""
 		if (self.motor_direction == 1):
+			# If we are already moving forward, there is no need to clear the motors. 
 			pass
 		elif (self.motor_direction == -1):
 			self.clear_motor()
 		self.write_motor(val,0)
-		self.motor_direction = 1
+		self.motor_direction = 1 # Update the direction
 		self.motor_speed = val
 
 	def write_backward(self,val):
+		""" Writes the set duty cycle to the motor in the reverse direction. Val should be between 0-100. 
+		"""
 		if (self.motor_direction == -1):
+			# If we are already moving backward, there is no need to clear the motor. 
 			pass
 		elif (self.motor_direction == 1):
 			self.clear_motor()
 		self.write_motor(0,val)
-		self.motor_direction = -1
+		self.motor_direction = -1 # Update the direction
 		self.motor_speed = val
 
 	def clear_motor(self):
+		""" Clear the motor by writing zero to both directions. 
+		"""
 		self.write_motor(0,0)
 		self.motor_direction = 0
 		self.motor_speed = 0
 
 	def run_0(self):
+		""" Run program 0: move the paddle back and forth to its edges. 
+		"""
 		if (self.intended_direction == 1):
 			if self.angle > self.max_angle:
 				self.intended_direction = -1
@@ -100,6 +136,8 @@ class user_functions:
 				print 4
 
 	def run_2(self):
+		""" Run program 2: move the paddle in the virtual damper configuration. 
+		"""
 		motor_speed = BASE_MOTOR_SPEED + (self.del_angle*10)
 		if (self.direction == 1):
 			self.write_backward(motor_speed)
@@ -110,22 +148,21 @@ class user_functions:
 			print ("direction: %d, motor_direction: %d, delta_angle: %f" % (self.direction,self.motor_direction,self.del_angle))
 			print 2
 
-	def run_wall(self):
+	def run_5(self):
+		""" Run program 5: wall configuration. 
+		"""
 		#variables for the wallsides.  stop is the outer limit, thresh is
 		#the point where it starts to bounce you back.
 		left_thresh = self.min_angle + 100
 
 		right_thresh = self.max_angle - 100
-		while True:
-			self.update_vals
-
-			if left_thresh < self.angle < right_thresh:
-				#Normal base case, don't do anything
-				pass
-			elif self.angle < left_thresh:
-				self.write_forward(100)
-			elif self.angle > right_thresh:
-				self.write_backward(100)
+		if left_thresh < self.angle < right_thresh:
+			#Normal base case, don't do anything
+			pass
+		elif self.angle < left_thresh:
+			self.write_forward(100)
+		elif self.angle > right_thresh:
+			self.write_backward(100)
 
 
 	def run_cycle(self,mode):
